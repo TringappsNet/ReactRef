4bb6d800a33c6f159ff968f67a0a6e3fa0322b3d
Siva-bharathiP <136304492+Siva-bharathiP@users.noreply.github.com>
2024-02-23 15:49:40 +0530

    TestCodeAdded

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
 // test('renders learn react link', () => {
 //   render(<App />);
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
+
+describe('YourFormComponent', () => {
+  it('renders label and input for "First Name"', () => {
+    render(<App />); 
+
+    const firstNameLabel = screen.getByText(/First Name/i);
+    const firstNameInput = screen.getByRole('textbox', { name: /First Name/i });
+
+    expect(firstNameLabel).toBeInTheDocument();
+    expect(firstNameInput).toBeInTheDocument();
+  });
+});
